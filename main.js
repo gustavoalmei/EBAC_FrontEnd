5648403d72cd2a3c157830dda3b1323cf46d2195
Gustavo Almeida <gustavo.almei2@hotmail.com>
2022-12-06 17:51:53 -0300

    commit initial

--- main.js
+++
@@ -1,8 +0,0 @@
-$('button').on('click',function(e){
-    e.preventDefault();
-    $('ul').append(`<li>${$('#task-input').val()}</li>`)
-})
-
-$('ul').on('click',function(e){
-    $(e.target).css("text-decoration","line-through")
-})
