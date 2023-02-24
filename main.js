8ea704a94ad46b01c32839fc18c7ab415d9dbd46
Gustavo Almeida <gustavo.almei2@hotmail.com>
2023-02-24 15:48:32 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const Pessoa = function (nome){
+    this.nome = nome;
+};
+
+const Carro = function(nome, marca, modelo, anoModelo, anoFabricacao){
+    Pessoa.call(this, nome);
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anoModelo = anoModelo;
+    this.anoFabricacao = anoFabricacao;
+    this.acelerar = function(){
+        console.log("VRUUUUUUM");
+    };
+    this.estacionado = function(){
+        console.log("Carro estacionado.");
+    };
+    this.explodido = function(){
+        console.log("MEU DEUS O CARRO EXPLODIU!!!!!");
+    };
+    this.mensagem = function(){
+        console.log(`${this.nome} possui um(a) ${this.modelo} da ${this.marca} ano ${this.anoModelo}.`);
+    };
+};
+
+const Gasolina = function(nome, marca, modelo, anoModelo, anoFabricacao){
+    Carro.call(this, nome, marca, modelo, anoModelo, anoFabricacao);
+    console.log(`${this.modelo} é um modelo que usa gasolina.`);
+};
+
+const Eletrico = function(nome, marca, modelo, anoModelo, anoFabricacao){
+    Carro.call(this, nome, marca, modelo, anoModelo, anoFabricacao);
+    console.log(`${this.modelo} é um modelo elétrico.`);
+};
+
+let pessoa1 = new Eletrico("Joãozinho", "Tesla", "Model S", 2023, 2022);
+pessoa1.mensagem();
+pessoa1.acelerar();
+pessoa1.explodido();
